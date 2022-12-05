fae06720865460fd8da38d9fe7f21b33421d6a2c
Katey69 <katekupica@inbox.lv>
2022-12-05 11:20:41 +0200

    get info working

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from options import reserve
+from options import reserve,get_info
 
 print("""Welcome to the menu!      
 Please choose an option: 
@@ -11,11 +11,14 @@ Please choose an option:
 izvele = int(input())
 if izvele == 1:
     reserve()
+elif izvele == 3:
+    print("""      
+    Please choose which info: 
 
-
-
-
-
-
-
+    1.Viesis info
+    2.Stay info
+    3.Room info
+    """)
+    izvele = int(input())
+    get_info(izvele)
 
--- options.py
+++ options.py
@@ -1,12 +1,54 @@
 import sqlite3
-
+from utility import dictfetchall
 connection = sqlite3.connect('viesnica.db')
 
 def reserve():
     with connection as con:
         cur = con.cursor()
         cur.execute("SELECT * FROM Istaba")
+        visas_istabas = dictfetchall(cur)
     con.close()
     print("Thank you for reserving!")
+    print(visas_istabas)
 
-reserve()
+
+def get_info(izvele):
+    if izvele == 1:
+        table_name = "Viesis"
+        visi_viesi = get_all_data(table_name)
+
+
+        print("----------------------------------------")
+        print("| vards | uzvards | tel_nr| studentsID |")
+        print("----------------------------------------")
+        for viesi in visi_viesi:
+            print("|", viesi["vards"], "|", viesi["uzvards"], "|", viesi["tel_nr"], "|", viesi["studentsID"])
+    elif izvele == 2:
+        table_name = "Uzturesanas"
+        visas_uzturesanas = get_all_data(table_name)
+
+        print("---------------------------------------------------------------")
+        print("| ierakstisanas_laiks | izrakstisanas_laiks | istaba | viesis |")
+        print("---------------------------------------------------------------")
+        for date in visas_uzturesanas:
+            print("|", date["ierakstisanas_laiks"], "|", date["izrakstisanas_laiks"], "|", date["istaba"], "|", date["viesis"])
+    else:
+        table_name = "Istaba"
+        visas_istabas = get_all_data(table_name)
+
+        print("--------------------------------------------")
+        print("| numurs | izmers | is_luxury | kapacitate |")
+        print("--------------------------------------------")
+        for istaba in visas_istabas:
+            print("|", istaba["numurs"], "|", istaba["izmers"], "|", istaba["is_luxury"], "|",
+                  istaba["kapacitate"])
+
+
+
+def get_all_data(table_name):
+    with connection as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM {name}".format(name=table_name))
+        visas_istabas = dictfetchall(cur)
+    con.close()
+    return visas_istabas
